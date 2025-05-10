ad3ee2916c6e8647db3fa9eb788058cf331c9c66
YongminGwon <clumsyrhymer@gmail.com>
2025-05-10 22:31:23 +0900

    set pch.h

---
+++ ServerCore/CorePch.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+/*----------------------
+    ServerCoreIncludes
+-----------------------*/
+#include "Types.h"
+
+/*----------------------
+         STL
+-----------------------*/
+#include <iostream>
+
+/*----------------------
+       Container
+-----------------------*/
+#include <list>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+
+/*----------------------
+
+-----------------------*/
