896cf5b8dee1889e1e9e9eeff62c3c83ad069117
YongminGwon <clumsyrhymer@gmail.com>
2025-05-13 17:11:36 +0900

    Basic socket program test

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -2,4 +2,37 @@
 
 int main()
 {
+	WSADATA wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		return 0;
+	}
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
+	}
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	/*serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");*/
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = htons(8000);
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)))
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Connect ErrorCode : " << errCode << endl;
+		return 0;
+	}
+	cout << "Connected To Server" << endl;
+
+	while (true)
+	{
+		this_thread::sleep_for(1s);
+	}
+
+	::closesocket(clientSocket);
+	::WSACleanup();
 }
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,22 +1,57 @@
 #include "pch.h"
 
-void ThreadMain()
-{
-	while (true)
-	{
-		cout << "Hello I'm Thread... " << LThreadId << endl;
-		this_thread::sleep_for(1s);
-	}
-}
 int main()
 {
-	auto& core = GlobalCore::Instance();
-	for (int32 i = 0; i < 5; i++)
+	WSADATA wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 	{
-		core.GetThreadManager().Launch(ThreadMain);
+		return 0;
 	}
 
-	core.GetThreadManager().Join();
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
+	}
 
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+	serverAddr.sin_port = ::htons(8000);
+
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	while (true)
+	{
+		SOCKADDR_IN clientAddr;
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrlen = sizeof(clientAddr);
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrlen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept ErrorCode : " << errCode << endl;
+			return 0;
+		}
+		char ipAddr[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddr, sizeof(ipAddr));
+		cout << "Client Connected! IP = " << ipAddr << endl;
+	}
+	::WSACleanup();
 	return 0;
 }
